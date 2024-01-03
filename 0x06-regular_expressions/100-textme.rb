cb4060adf08e6641242feadd8bd92737f849625b
Tijani Sheu <110635002+iAhm-codes@users.noreply.github.com>
2024-01-03 21:15:22 +0100

    Update: corrected expression implementation100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-List = [Format[0].compact, Format[1].compact, Format[2].compact]
-puts List.join(',')
+puts ARGV[0]
+ .scan(/(?<=from:|to:|flags:).*?(?=\])/)
+.join(',')
